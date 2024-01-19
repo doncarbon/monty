d39288871b2d2a25012faea81d52d11b91e1ecb1
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-18 16:42:44 -0800

    sub first draft

---
+++ sub.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+/**
+ * f_sub - subtracts the top element of the stack
+ * from the second top element of the stack.
+ *
+ * Return: Nothing (void)
+ */
+void f_sub(stack_t **stack, unsigned int line_number)
+{
+}
