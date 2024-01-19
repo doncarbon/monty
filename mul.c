218a422af44348577064e7429ba6b80b4cada3b1
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-18 17:12:27 -0800

    fix betty style

--- mul.c
+++ mul.c
@@ -6,12 +6,11 @@
  */
 void f_mul(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    (*stack)->next->n *= (*stack)->n;
-    f_pop(stack, line_number);
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n *= (*stack)->n;
+	f_pop(stack, line_number);
 }
