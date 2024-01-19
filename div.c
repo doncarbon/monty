4a731b07e1fabba62cad06bea38695c2b520a757
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-18 17:00:04 -0800

    fix betty style

--- div.c
+++ div.c
@@ -9,17 +9,15 @@
 void f_div(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    if ((*stack)->n == 0)
-    {
-        fprintf(stderr, "L%u: division by zero\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    (*stack)->next->n /= (*stack)->n;
-    f_pop(stack, line_number);
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n /= (*stack)->n;
+	f_pop(stack, line_number);
 }
