9d1acf31956a7e37f85b0d5c5728cc52150f8d16
5alidev <khalidbenhamou.dev@outlook.com>
2024-01-18 16:48:57 -0800

    betty style

--- sub.c
+++ sub.c
@@ -8,11 +8,10 @@
 void f_sub(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
-        {
-                fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-
-        (*stack)->next->n -= (*stack)->n;
-        f_pop(stack, line_number);
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n -= (*stack)->n;
+	f_pop(stack, line_number);
 }
